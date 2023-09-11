b7a86482a2b92fba60f6df267520cd08728f5057
Gabriela Dourado <gdourado.066@gmail.com>
2023-09-10 22:43:46 -0300

    Add files via upload

---
+++ projetoefg.js
@@ -0,0 +1,7 @@
+
+var typed = new typed("span",{
+    strings:["Gabriela Dourado.","Desenvolvedora Web e Mobile.","UX/UI Designer.", "Software Engineer Student at University Of Brasília","Speaker."],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true
+});
